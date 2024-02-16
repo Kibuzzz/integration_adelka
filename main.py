80ba67c43eb9dc6bae7e76ecdba32fe2fd28e40d
Pablo <pablo@Pablos-MacBook-Air.local>
2024-02-16 14:40:02 +0300

    mvp

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from datetime import datetime as dt
 
 class User:
 
+    ACTIVE = 1
+    NOT_ACTIVE = 0
+
     count_users = 0
 
     def __init__(self, name: str) -> None:
-        self.status = 1  # 1 - Активный пользователь, 0 - Неактивный пользователь
+        self.status = User.ACTIVE  # 1 - Активный пользователь, 0 - Неактивный пользователь
         self.name = name
         self.id = User.count_users
         self.status = User.count_users
@@ -19,18 +22,16 @@ class User:
 
 class Event:
 
+    ACTUAL = 1
+    NOT_ACTUAL = 0
+
     def __init__(self, name: str, date_string: str) -> None:
         self.name = name
-        self.status = 1  # 1 - Актуальны, 0 - Неактуальный
+        self.status = Event.ACTUAL  # 1 - Актуальны, 0 - Неактуальный
         self.event_date = dt.strptime(date_string, '%d.%m.%y %H:%M')
 
     def __str__(self) -> str:
-        str_status = ""
-        if self.status:
-            str_status = "Актуально"
-        else:
-            str_status = "Не актуально"
-
+        str_status = "Актуально" if self.status else "Не актуально"
         return "{} {} {}".format(self.name, self.event_date, str_status)
 
 
@@ -39,8 +40,8 @@ events: list[Event] = []
 # Добавить мероприятие и купить на него билет
 
 
-def addEvent(name_event: str, date_string: str) -> None:
-    new_event = Event(name_event, date_string)
+def addEvent(event_name: str, date_string: str) -> None:
+    new_event = Event(event_name, date_string)
     events.append(new_event)
 
 
@@ -59,12 +60,42 @@ def getActualEvents() -> list[Event]:
     return [event for event in events if event.status == 1]
 
 # Прислать напоминание о мероприятии
-# Прислать выгрузку о посещении мероприятий конкретным пользователем
 
 
+def sentNotifications(event_name: str) -> None:
+    event = [e for e in events if e.name == event_name][0]
+    if event:
+        print(
+            f"Напоминание: Скоро начнется мероприятие '{event.name}' в {event.event_date}")
+    else:
+        print("Мероприятие не найдено")
+
+# Выгрузка о посещении мероприятий конкретным пользователем
+
+
+def userAttendance(user: User) -> None:
+    return user.tickets
+
+
+# Создание пользователя
 u = User("kiril")
-e = Event("Новогодняя елка", "31.12.23 12:48")
-events.append(e)
+
+# Добавление мероприятий
+addEvent("Новогодняя елка", "31.12.23 12:48")
 addEvent("Дискотека", "13.01.24 14:50")
-for e in events:
-    print(e)
+
+# Покупка билетов
+buyTicket(u, "Новогодняя елка")
+buyTicket(u, "Дискотека")
+
+# Получение списка актуальных мероприятий
+print("Актуальные мероприятия:")
+for event in getActualEvents():
+    print(event)
+
+# Отправка уведомления о мероприятии
+sentNotifications("Дискотека")
+
+# Получение списка мероприятий, которые посетил пользователь
+print("Посещенные мероприятия пользователем:")
+print(userAttendance(u))
